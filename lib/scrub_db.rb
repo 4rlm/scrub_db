c46770eee47cece55d1f5b7b1e7008ff8bc0f5c6
Adam Booth <4rlm@protonmail.ch>
2018-07-01 12:38:07 -0500

    Added Travis CI

--- lib/scrub_db.rb
+++ lib/scrub_db.rb
@@ -2,8 +2,8 @@ require "scrub_db/version"
 require 'scrub_db/webs'
 require 'scrub_db/strings'
 require 'scrub_db/filter'
-require 'pry'
 require 'crm_formatter'
+# require 'pry'
 
 module ScrubDb
 
